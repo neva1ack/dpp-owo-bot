86ae1cb153094fbc5f54ce0632721f74ec5d23a7
neva1ack <zanebanks2@gmail.com>
2022-09-21 15:39:06 +0930

    refactor: buildsystem

--- unix_cmake_ninja_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# generate cmake buildsystem
-cmake -S . -B build -G Ninja
-# build cmake project
-cmake --build build -j 4
-# Check if symbolic link for compile_commands.json, if not create it
-if ! [ -L "./compile_commands.json" ]; then
-  ln -s build/compile_commands.json compile_commands.json
-fi
